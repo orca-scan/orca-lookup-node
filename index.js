9c0fcc285163e77e3011698d0ddaff896df68f00
John Doherty <contact@johndoherty.info>
2020-10-10 20:26:24 +0100

    Added express example of handling a lookup request

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+// GET / handler
+app.get('/', function (req, res) {
+
+    // get the incoming barcode sent from Orca Scan (scanned by a user)
+    var barcode = req.query.barcode;
+
+    // TODO: query a database or API to retrieve some data based on barcode value
+
+    // create an object with data to return (property names must match Orca column names)
+    var dataToReturn = {
+        Vin: barcode,
+        Make: 'SUBARU',
+        Model: 'Legacy',
+        ManufacturerName: 'FUJI HEAVY INDUSTRIES U.S.A',
+        VehicleType: 'PASSENGER CAR',
+        Year: 1992
+    };
+
+    // return data to Orca as a JSON object
+    res.json(dataToReturn);
+});
+
+// start the server on port 5000
+app.listen(5000);
